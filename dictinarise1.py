18a4a16e6a53ae14309e65ff86b2bb11661b1474
Md zubayer <junayed75489@gmail.com>
2025-06-10 07:30:29 +0600

    dictionary_v1.1.1

--- dictinarise1.py
+++ dictinarise1.py
@@ -51,4 +51,115 @@ print(y)
 # check if key Exists
 if 'name' in dic4:
     print('Yes, "name" is one of the keys in the dic4 dictionary')
-    
+
+# Change Value
+dic4['name'] = 'abdullah'
+print(dic4)
+
+dic4.update({'city': 'borishall'})
+print(dic4)
+# Removing Items
+# The pop() method removes the item with the specified key name:
+
+dic4.pop('color')
+print(dic4)
+
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+dic4.popitem()
+print(dic4)
+# del dic4
+dic4.clear()
+print(dic4)
+
+# Loop through a dictionary
+# get keys
+for x in dic3:
+    print(x)
+for x in dic3.keys():
+    print(x)
+# get values
+for x in dic3:
+    print(dic3[x])
+for x in dic3.values():
+    print(x)
+# get keys and values
+for x, y in dic3.items():
+    print(x, y)
+    print(x)
+    print(y)
+
+# Copy a dictionary
+dict3 = dic3.copy()
+print(dict3)
+
+# using dict()
+dict4 = dict(dic3)
+print(dict4)
+
+#Nested Dictionary
+# way 1
+myfamily = {
+    'child1': {
+        'name': 'Emil',
+        'age': 22
+    },
+    'child2': {
+        'name': 'ahmad',
+        'age': 33
+    },
+    'child3': {
+        'name': 'hasan',
+        'age': 44
+    }
+} 
+
+# way 2
+cl1 = {
+    'name': 'abdullah',
+    'age': 11
+}
+cl2 = {
+    'name': 'shohel',
+    'age': 23
+}
+cl3 = {
+    'name': 'ahsan',
+    'age': 12
+}
+
+myfamily2 = {
+    'child4': cl1,
+    'child5': cl2,
+    'child6': cl3
+}
+
+# Access items in Nested dictionary
+print(myfamily['child1']['name'])
+print(myfamily2['child4']['name'])
+
+# Loop Through Nested dictionary
+for x , obj in myfamily.items():
+    print(x)
+    for y in obj:
+        print(y + ':', obj[y])
+for x, obj in myfamily2.items():
+    print(x)
+    for y in obj:
+        print(obj[y])
+
+# dictionary formkeys()
+k = ('key1', 'key2', 'key3' )
+v = 12
+d = dict.fromkeys(k,v)
+print(d)
+ks = dict.fromkeys(k)
+print(ks)
+x = d.get('key1')
+print(x)
+x = d.get('key4', 333)
+print(x)
+
+z = d.setdefault('key1', 100)
+print(z)
+zs = d.setdefault('key4', 1000)
+print(zs)
